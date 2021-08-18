3c373fcef0ca476555d4aca8cff283a5e228e38a
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-08-18 19:34:05 -0400

    Components - Modal - receiving setResetGame and set it to true on click event

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export const Modal = ({ setCompleted }) => {
+export const Modal = ({ setResetGame }) => {
   return(
     <div className="modal">
       <div className="moda__container">
         <span className="modal__icon"></span>
         <h2 className="modal__heading">Game Copleted</h2>
-        <button className="modal__btn" onClick={() => setCompleted(false)}>Start Over</button>
+        <button className="modal__btn" onClick={() => setResetGame(true)}>Start Over</button>
       </div>
     </div>
   )
