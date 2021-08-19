ac8f01d0bf002bd2f5b59dd1f98509f10c99f65b
Maikol Hernandez <maikol_hernandez_rm@hotmail.com>
2021-08-18 20:52:44 -0400

    Project - App - adding some comments to improve code readablity

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Card, Modal } from './components'
 
 function App() {
+  // APP STATE
   const [cards, setCards] = useState([
     {id: 1, framework: 'angular.svg' },
     {id: 2, framework: 'aurelia.svg' },
@@ -16,6 +17,7 @@ function App() {
   const [isLocked, setIsLocked] = useState(false)
   const [completed, setCompleted] = useState(false)
   const [resetGame, setResetGame] = useState(false)
+  // HANDLE CARDS
   const handleCards = (action, cards, delay) => {
     action === 'unflip-cards' ? setTimeout(() => {
       cards.map(card => card.setFlipped(false))
@@ -23,24 +25,29 @@ function App() {
     }, delay) : setIsLocked(false)
     setMatches([])
   }
+  // HANDLE MATCHES
   const handelMatches = () => {
     setIsLocked(true)
     const match = matches[0].id === matches[1].id
     !match ? handleCards('unflip-cards', matches, 500) : handleCards('none')
     if (match && !flippedCards.find(el => el.id === matches[0].id)) setFlippedCards(prev => prev.concat(matches))
   }
+  // HANDLE RESET
   const handleReset = () => {
     handleCards('unflip-cards', flippedCards, 0)
     setFlippedCards([])
     setResetGame(false)
     setCompleted(false)
   }
+  // WATCHING FOR MATCHES CHANGES
   useEffect(() => {
     if (matches.length && matches.length === 2) handelMatches()
   }, [matches])
+  // WATCHING FOR FLIPPEDCARDS CHANGES
   useEffect(() => {
     if (flippedCards.length === (cards.length * 2)) setCompleted(true)
   }, [flippedCards])
+  // WATCHING FOR RESETGAME CHANGES
   useEffect(() => {
     if (resetGame) handleReset()
   }, [resetGame])
